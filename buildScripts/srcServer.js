c5eb9eb5ec39d0833b9db4911970b8b521420ef1
Walter Reyes <wal.reyes.11@gmail.com>
2018-01-30 15:16:28 -0600

    HTTP Calls

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -21,6 +21,14 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+  res.json([
+    {id: 1, firstName: 'Bob', lastName: 'Smith', email: 'bob@gmail.com'},
+    {id: 2, firstName: 'Bob', lastName: 'Smith', email: 'bob@gmail.com'},
+    {id: 3, firstName: 'Bob', lastName: 'Smith', email: 'bob@gmail.com'},
+  ]);
+});
+
 app.listen(port, function(err) {
   if (err) {
     console.log(err)
